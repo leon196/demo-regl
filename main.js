c597941eb006d1d8c9a54fed6597f2397e27a43c
Leon Denise <leon.denise@free.fr>
2021-03-29 12:03:17 +0200

    basic regl with blender html5 addon

---
+++ main.js
@@ -0,0 +1,32 @@
+const regl = require('regl')()
+const BlenderWebSocket = require('./BlenderWebSocket')
+const animationData = require('./animation.json')
+const BlenderHTML5Animations = require('./blender-html5-animations.min')
+const neon = require('./neon')(regl)
+const camera = require('./camera')(regl, {
+    center: [0,0,0],
+    distance: 4
+})
+
+console.log(animationData);
+var animations = new BlenderHTML5Animations.ActionLibrary(animationData);
+
+var elapsed = 0;
+
+var blenderSocket = new BlenderWebSocket();
+blenderSocket.addListener('frame', function(frame)
+{
+    elapsed = frame / 24;
+});
+
+regl.frame(function () {
+    regl.clear({
+        color: [0, 0, 0, 1]
+    })
+    camera(() => {
+        neon({ time: regl.now() })
+    })
+
+    var position = animations['CameraAction'].paths['location'].evaluate(elapsed);
+    // console.log(position);
+})
